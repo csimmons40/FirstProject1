20b33bba0b6a6201ab693094a6385cd07101b205
Craig Simmons <craig.simmons@intoglobal.com>
2020-01-02 14:38:47 +0000

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject1
+{
+    class Car
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It can be driven forward"+"And"+ "It can be driven backward");
+        }
+    }
+}
